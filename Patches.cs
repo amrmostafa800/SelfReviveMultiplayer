c01b3886b66d6323c054b2981b04861d51a54ab0
amr <amrmostafa800@gmail.com>
2023-11-10 13:37:47 +0200

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace SelfReviveMultiplayer
+{
+    public class MuckPatch
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(GraveInteract), nameof(GraveInteract.Update))]
+        public static void Update(ref GraveInteract __instance, ref int ___id)
+        {
+            if (__instance.timeLeft <= 0f)
+            {
+                ClientSend.RevivePlayer(__instance.playerId, ___id, grave: true);
+            }
+        }
+
+    }
+}
---
+++ SelfReviveMultiplayer.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace SelfReviveMultiplayer
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class SelfReviveMultiplayer : BaseUnityPlugin
+    {
+        public const string
+            GUID = "SelfReviveMultiplayer",
+            NAME = "SelfReviveMultiplayer",
+            VERSION = "1.0.1";
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(MuckPatch));
+            Logger.LogMessage("Loaded SelfReviveMultiplayer");
+        }
+    }
+}
